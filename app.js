86d4be403abe4d9a272b13d2276a48a923f33723
petravarga <60893081+petravarga@users.noreply.github.com>
2020-09-21 13:46:01 +0200

    initial files

---
+++ app.js
@@ -0,0 +1,45 @@
+/*
+var btnDanger = document.getElementById('btndanger');
+
+btnDanger.addEventListener('click', buttonClicked)
+
+function buttonClicked(e) {
+    var x = event.offsetX;
+    var y = event.offsetY;
+
+    var backColor = `rgb(${x}, ${y}, 0)`;
+
+    document.getElementById('body').style.backgroundColor = backColor;
+
+    console.log(x);
+    console.log(y);
+
+}
+*/
+/*
+var cardCon = document.getElementById('cardcontainer');
+
+cardCon.addEventListener('mouseenter', mouseEntered)
+
+function mouseEntered() {
+    document.getElementById('body').style.backgroundColor = "blue";
+
+    setTimeout(changeColor, 3000);
+}
+
+
+function changeColor() {
+    document.getElementById('body').style.backgroundColor = "red";
+}
+*/
+/*
+
+var heading = document.getElementsByClassName('display-3');
+
+    heading[0].addEventListener('click', changeCol)
+
+    function changeCol() {
+        heading[0].style.color = "blue";
+    }
+
+ console.log(heading); */
